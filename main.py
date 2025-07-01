3ac513ab124ed2253dd9ca5c952d0c38aeb9a853
Esmail Fawzy <esmail.elshishtawy.2001@gmail.com>
2025-07-01 14:06:13 +0300

    fix issue

--- main.py
+++ main.py
@@ -431,4 +431,5 @@ def api_predict():
 
 if __name__ == "__main__":
     load_model_and_scaler()
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True)
